6e1d60fd051b85ffb6b0224f24c56eea044032d9
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-10-28 22:22:11 +0200

    popravio

---
+++ 01-csahrp-procedural/01-instuctions/03-comments/Program.cs
@@ -0,0 +1,29 @@
+﻿// This is an example of single-line comment
+
+// The foolowing statement prints text "Mickey Mouse" on conole
+System.Console.WriteLine("Mickey Mouse");
+
+System.Console.WriteLine("Donald Duck"); // this prints "Donald Duck"
+
+System.Console.WriteLine(); // this prints end-of-line character 
+
+/*
+
+This is an example of the comment
+
+    that is extend
+
+        in multiple lines
+
+*/
+
+/* One statement can also be written in multiple lines */
+
+System.
+    Console.
+        WriteLine(
+            "James Dean");
+
+
+// Many statements can be writen in the same line
+System.Console.WriteLine("Goofy"); System.Console.WriteLine("Miney Mouse"); System.Console.WriteLine();
---
+++ 01-csahrp-procedural/02-variables/03-byte-short-long-variables/ByteShortLong.cs
@@ -0,0 +1,50 @@
+﻿byte xb = 100;
+byte yb = 100;
+System.Console.WriteLine("xb = " + xb);
+System.Console.WriteLine("yb = " + yb);
+byte zb = (byte)(xb + yb);
+System.Console.WriteLine("xb + yb = " + zb);
+System.Console.WriteLine();
+
+xb = 100;
+yb = 200;
+System.Console.WriteLine("xb = " + xb);
+System.Console.WriteLine("yb = " + yb);
+zb = (byte)(xb + yb);
+System.Console.WriteLine("xb + yb = " + zb);
+System.Console.WriteLine();
+
+short xs = xb;
+short ys = yb;
+System.Console.WriteLine("xs = " + xs + ", ys = " + ys);
+short zs = (short)(xs + ys);
+System.Console.WriteLine("xs + ys = " + zs);
+System.Console.WriteLine();
+
+xs = 30000;
+ys = 5_000;
+System.Console.WriteLine("xs = " + xs + ", ys = " + ys);
+zs = (short)(xs + ys);
+System.Console.WriteLine("xs + ys = " + zs);
+System.Console.WriteLine();
+
+xs = 30000;
+ys = 30_000;
+System.Console.WriteLine("xs = " + xs + ", ys = " + ys);
+zs = (short)(xs + ys);
+System.Console.WriteLine("xs + ys = " + zs);
+System.Console.WriteLine();
+
+long xl = xs;
+long yl = ys;
+System.Console.WriteLine("xl = " + xl + ", yl = " + yl);
+long zl = xl + yl;
+System.Console.WriteLine("xl + yl = " + zl);
+System.Console.WriteLine();
+
+xl = 5_000_000_000_000_000_000;
+yl = xl;
+System.Console.WriteLine("xl = " + xl + ", yl = " + yl);
+zl = xl + yl;
+System.Console.WriteLine("xl + yl = " + zl);
+System.Console.WriteLine();
