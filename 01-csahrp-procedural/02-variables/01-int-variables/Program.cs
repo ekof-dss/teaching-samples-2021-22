47511e1386346a1f4d9e9d47f7509e0f38b91ff2
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-10-28 19:29:08 +0200

    added projects into solution

--- 01-csahrp-procedural/02-variables/01-int-variables/Program.cs
+++ 01-csahrp-procedural/02-variables/01-int-variables/Program.cs
@@ -1,6 +1,11 @@
 ﻿int x = 7;
 System.Console.WriteLine("x = " + x);
-int y = 9;
+
+int y = 4+3;
 System.Console.WriteLine("y = " + y);
+
 int z = x + y;
 System.Console.WriteLine("x + y = " + z);
+
+int t = (int) 3.5;
+System.Console.WriteLine("t = " + t);
--- 01-csahrp-procedural/02-variables/02-int-variables overflow/Program.cs
+++ 01-csahrp-procedural/02-variables/02-int-variables overflow/Program.cs
@@ -1,6 +1,8 @@
 ﻿int x = 1_500_000_000;
 System.Console.WriteLine("x = " + x);
+
 int y = 1_500_000_000;
 System.Console.WriteLine("y = " + y);
+
 int z = x + y;
 System.Console.WriteLine("x + y = " + z);
