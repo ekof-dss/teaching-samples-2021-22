cd0c2011ab8adfbc7750d757be96fc4fe8ceb595
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-11-21 14:05:36 +0100

    popravio

--- 04-blazor-wasm/01-fundametals/08-multiple-pages/Program.cs
+++ 04-blazor-wasm/01-fundametals/08-multiple-pages/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace _01_wasm_hello
+namespace _08_multiple_pages
 {
     public class Program
     {
