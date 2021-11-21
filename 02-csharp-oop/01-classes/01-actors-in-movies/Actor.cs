6466e337432f672005c0f3c787522b678180b92b
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-11-21 15:08:27 +0100

    popravio

---
+++ 02-csharp-oop/01-classes/01-actors-in-movies/Actor.cs
@@ -0,0 +1,11 @@
+namespace s01_actors_in_movies
+{
+    public class Actor
+    {
+        public string lastName;
+
+        public string firstName;
+
+        public double imdbMark = 0;
+    }
+}
---
+++ 02-csharp-oop/01-classes/01-actors-in-movies/Program.cs
@@ -0,0 +1,12 @@
+﻿using s01_actors_in_movies;
+
+System.Console.WriteLine("Actors");
+System.Console.WriteLine();
+
+Actor actor = new Actor(){firstName = "John", lastName = "Goodman", imdbMark = 9.7};
+System.Console.WriteLine($"Actor: {actor.firstName} {actor.lastName}, imdb:{actor.imdbMark}");
+System.Console.WriteLine();
+
+Actor actor2 = new Actor(){lastName = "De Niro", imdbMark = 9.4, firstName = "Robert"};
+System.Console.WriteLine($"Actor: {actor2.firstName} {actor2.lastName}, imdb:{actor2.imdbMark}");
+System.Console.WriteLine();
