cf3e53c277df43d219e3785690c90492e0e1617e
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-11-30 20:24:25 +0100

    rename some folders

--- project/07-for/Program.cs
+++ project/07-for/Program.cs
@@ -2,13 +2,14 @@
 int n = int.Parse(System.Console.ReadLine());
 
 int i = n;
-while(i>=1){
+while (i >= 1)
+{
     System.Console.WriteLine("Number " + i);
-    i = i - 1; 
+    i = i - 1;
 }
 System.Console.WriteLine("GO");
 
-int i = n;
-for( ; i>=1; i--)
+i = n;
+for (; i >= 1; i--)
     System.Console.WriteLine("Number " + i);
 System.Console.WriteLine("GO");
