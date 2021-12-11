05367abfde02f78809b012c1f9a80b31eb05e20a
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-12-11 15:48:30 +0100

    actors-service-data

--- 04-blazor-wasm/02-movie-actors/08-actors-service-data/Program.cs
+++ 04-blazor-wasm/02-movie-actors/08-actors-service-data/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+using project.Services;
+
 namespace project
 {
     public class Program
@@ -17,7 +19,10 @@ namespace project
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => 
+                new HttpClient { BaseAddress = new Uri(
+                    builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<IActorService, ActorService>();
 
             await builder.Build().RunAsync();
         }
