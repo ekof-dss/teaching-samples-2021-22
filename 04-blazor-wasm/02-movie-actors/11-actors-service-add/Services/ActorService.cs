2f1b27c6e811dbbe4f04683b22794038b4a7ed92
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-12-11 21:29:41 +0100

    actors-service-add

---
+++ 04-blazor-wasm/02-movie-actors/11-actors-service-add/Services/ActorService.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+using project.Models;
+
+namespace project.Services
+{
+    public class ActorService : IActorService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IMessagingService _messagingService;
+        private Actor[] _actors = null;
+        public ActorService(HttpClient httpClient, IMessagingService messagingService)
+        {
+            _httpClient = httpClient?? throw new ArgumentNullException(
+                nameof(httpClient));
+            _messagingService = messagingService?? throw new 
+                ArgumentNullException(nameof(messagingService));
+        }
+
+        public Task<Actor[]> Add(string firstName, string lastName, 
+            string country)
+        {
+            List<Actor> actorList = new List<Actor>(_actors);
+            long maxId = actorList.Max(actor => actor.Id)+1;
+            Actor newActor = new Actor()
+            {
+                Id = maxId,
+                FirstName = firstName,
+                LastName = lastName,
+                CountryCode = country
+            };
+            actorList.Add(newActor);
+            _actors = actorList.ToArray();
+            return Task.FromResult(_actors);
+        }
+
+        public async Task<Actor[]> GetActors()
+        {
+            await _messagingService.Add("ActorsService::Actors fetched");
+            _actors = await _httpClient.GetFromJsonAsync<Actor[]>(
+                "sample-data/actors.json");
+            return _actors;
+        }
+    }
+}
---
+++ 04-blazor-wasm/02-movie-actors/11-actors-service-add/Services/IActorService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+
+using project.Models;
+
+namespace project.Services
+{
+    public interface IActorService
+    {
+        Task<Actor[]> GetActors();
+
+        Task<Actor[]> Add(string firstName, string lastName, string country);
+    }
+}
