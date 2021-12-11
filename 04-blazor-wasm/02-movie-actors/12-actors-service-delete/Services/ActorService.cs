875de2e92efb05bb1aa4e9a84ec4d12e10eeda36
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-12-11 21:47:51 +0100

    actors-service-delete

---
+++ 04-blazor-wasm/02-movie-actors/12-actors-service-delete/Services/ActorService.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+using project.Models;
+
+namespace project.Services
+{
+    public class ActorService : IActorService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IMessagingService _messagingService;
+        private Actor[] _actors = null;
+        public ActorService(HttpClient httpClient, IMessagingService messagingService)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(
+                nameof(httpClient));
+            _messagingService = messagingService ?? throw new
+                ArgumentNullException(nameof(messagingService));
+        }
+
+        public async Task<Actor[]> GetActors()
+        {
+            await _messagingService.Add("ActorsService::Actors fetched");
+            _actors = await _httpClient.GetFromJsonAsync<Actor[]>(
+                "sample-data/actors.json");
+            return _actors;
+        }
+
+
+        public Task<Actor[]> Add(string firstName, string lastName,
+            string country)
+        {
+            List<Actor> actorList = new List<Actor>(_actors);
+            long maxId = actorList.Max(actor => actor.Id) + 1;
+            Actor newActor = new Actor()
+            {
+                Id = maxId,
+                FirstName = firstName,
+                LastName = lastName,
+                CountryCode = country
+            };
+            actorList.Add(newActor);
+            _actors = actorList.ToArray();
+            return Task.FromResult(_actors);
+        }
+
+        public Task<Actor[]> Delete(Actor actor)
+        {
+            List<Actor> actorList = new List<Actor>(_actors);
+            actorList.Remove(actor);
+            _actors = actorList.ToArray();
+            return Task.FromResult(_actors);
+        }
+    }
+}
---
+++ 04-blazor-wasm/02-movie-actors/12-actors-service-delete/Services/IActorService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading.Tasks;
+
+using project.Models;
+
+namespace project.Services
+{
+    public interface IActorService
+    {
+        Task<Actor[]> GetActors();
+
+        Task<Actor[]> Add(string firstName, string lastName, string country);
+
+        Task<Actor[]> Delete(Actor actor);
+    }
+}
