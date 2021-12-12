1f45c731c890bc1b3ed998426d0d9dceff073a22
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-12-12 13:37:34 +0100

    todo persist on server

---
+++ 05-rest/03-actors-server-persist/rest-service/Controllers/ActorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using project.Data;
+using project.Models;
+using project.ViewModels;
+
+
+namespace project.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActorController : ControllerBase
+    {
+        private readonly ILogger<ActorController> _logger;
+
+        public ActorController(ILogger<ActorController> logger)
+        {
+            _logger = logger?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public IEnumerable<ActorDTO> Get()
+        {
+            var rng = new Random();
+            List<Actor> actors = ActorRepository.List( CountryRepostory.List() );
+            int skip = rng.Next(0, actors.Count/2);
+            int take = rng.Next(0, actors.Count);
+            return actors.Skip(skip).Take(take).Select(a => 
+            new ActorDTO(
+                a.Id,
+                a.FirstName,
+                a.LastName,
+                a.Country.Code,
+                a.DateOfBirth
+            )).ToList();
+        }
+    }
+}
